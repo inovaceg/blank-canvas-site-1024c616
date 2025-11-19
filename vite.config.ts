3bb015b43c7f1ef4a1fc866148fce094eded39e5
Felip Cal <inovaceg@gmail.com>
2025-11-19 02:10:43 -0300

    [dyad] Fixing Supabase environment variable error - deleted 1 file(s)

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [dyadComponentTagger(), react()],
-});
